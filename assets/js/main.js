1a765a446a100d4f15ac82d26b417148428855e1
Liviu Zachin <liviuzachin@yahoo.com>
2022-07-19 19:42:20 +0200

    final serie

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,36 @@ import Typed from 'typed.js';
     el.addEventListener('scroll', listener)
   }
 
+  /* Activeaza linkurile cand suntem intr-o sectiune */
+  const navLinks = select('.nav--main .nav-link', true)
+  const activateNavLinks = () => {
+    let position = window.scrollY + 200
+    navLinks.forEach(navLink => {
+      if (!navLink.hash) return
+      let section = select(navLink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navLink.classList.add('active')
+      } else {
+        navLink.classList.remove('active')
+      }
+    })
+  }
+
+  // listeners
+  window.addEventListener('load', activateNavLinks)
+  onScroll(document, activateNavLinks)
+
+  /* Scrool to hash */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      const el = select(window.location.hash) 
+      if (el) {
+        el.scrollIntoView()
+      }
+    }
+  })
+
   /* Toggle .header--scrolled  class to #header when page is scrolled */
   const selectedHeader = select('#header')
   if (selectedHeader) {
@@ -70,6 +100,19 @@ import Typed from 'typed.js';
       backDelay: 2000,
     })
   }
-
   
+  /* Back to top */
+  const backToTop = select('.back-to-top')
+  if (backToTop) {
+    const toggleBackToTop = () => {
+      if (window.scrollY > 100) {
+        backToTop.classList.add('active')
+      } else {
+        backToTop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBackToTop)
+    onScroll(document, toggleBackToTop)
+  }
+
 })()
